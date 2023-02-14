149e02e3448180b9f465b1065a581d23aab9d488
J0hn-4ndr3w <118709598+J0hn-4ndr3w@users.noreply.github.com>
2023-02-14 10:29:30 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,116 +1,127 @@
+function nextLoad(){
 
-
-/*----BTC----*/
-function getBitcoinPrice() {
-    const url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD';
-  
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      document.querySelector("#bitcoin-price").innerHTML = `BTC $${data.USD} `;
-    });
-  }
-  getBitcoinPrice();
-
-
-  /*----ETH----*/
-  
-  function getETHPrice() {
-          const xhr = new XMLHttpRequest();
-          xhr.open("GET", "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD", true);
-          xhr.onreadystatechange = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-              const data = JSON.parse(xhr.responseText);
-              document.getElementById("ethPrice").innerHTML = `ETH $${data.USD}`;
-            }
-          };
-          xhr.send();
-        }
-        getETHPrice();
-        
-       
-        
-  /*----LTC----*/
-        
-        function getLTCPrice() {
-          var xhr = new XMLHttpRequest();
-          xhr.open("GET", "https://api.coingecko.com/api/v3/simple/price?ids=litecoin&vs_currencies=usd", true);
-          xhr.onreadystatechange = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-              var data = JSON.parse(xhr.responseText);
-              var ltcPrice = data.litecoin.usd;
-              document.getElementById("ltc-price").innerHTML = "LTC $" + ltcPrice;
-            }
-          };
-          xhr.send();
-        }
-        
-        getLTCPrice();
-        
-  
-  /*----DOGE----*/
-          const url = 'https://min-api.cryptocompare.com/data/price?fsym=DOGE&tsyms=USD';
-  
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById("doge-price").innerHTML = `DOGE $${data.USD}`;
-    });
+  /*----BTC----*/
+  function getBitcoinPrice() {
+      const url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD';
     
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        document.querySelector("#bitcoin-price").innerHTML = `BTC $${data.USD} `;
 
+        const standardCurrency = 21744.664925;
+        const currentPrice = `${data.USD}`;
+        const change = currentPrice - standardCurrency;
+        const percentage = (change / standardCurrency) * 100;
+        document.querySelector("#percentage").innerHTML = `(${percentage.toFixed(2)}%)`;
+      });
 
-    /*----YFI----*/
-    function getYFIPrice() {
-    const url = 'https://min-api.cryptocompare.com/data/price?fsym=YFI&tsyms=USD';
-  
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      document.querySelector("#YFI-price").innerHTML = `YFI $${data.USD}`;
-    });
-    };
-    getYFIPrice();
+      
+    }
+    getBitcoinPrice();
   
   
-  /*----LINK----*/
-  function getLinkPrice(){
-    const url = 'https://min-api.cryptocompare.com/data/price?fsym=LINK&tsyms=USD';
-  
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      document.querySelector("#link-price").innerHTML = `LINK $${data.USD}`;
-    });
-  }
-  getLinkPrice();
+    /*----ETH----*/
+    
+    function getETHPrice() {
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD", true);
+            xhr.onreadystatechange = function() {
+              if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                const data = JSON.parse(xhr.responseText);
+                document.getElementById("ethPrice").innerHTML = `ETH $${data.USD}`;
 
-  function getLink(){
-    const url = 'https://min-api.cryptocompare.com/data/price?fsym=LINK&tsyms=USD';
+                const standardCurrency = 1486.28;
+                const currentPrice = `${data.USD}`;
+                const change = currentPrice - standardCurrency;
+                const percentage = (change / standardCurrency) * 100;
+                document.querySelector("#percentageEth").innerHTML = `(${percentage.toFixed(2)}%)`;
+              }
+            };
+            xhr.send();
+          }
+          getETHPrice();
+          
+         
+          
+    /*----LTC----*/
+          
+          function getLTCPrice() {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "https://api.coingecko.com/api/v3/simple/price?ids=litecoin&vs_currencies=usd", true);
+            xhr.onreadystatechange = function() {
+              if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText);
+                var ltcPrice = data.litecoin.usd;
+                document.getElementById("ltc-price").innerHTML = "LTC $" + ltcPrice;
+
+                const standardCurrency = 91.10;
+                const currentPrice = `${data.litecoin.usd}`;
+                const change = currentPrice - standardCurrency;
+                const percentage = (change / standardCurrency) * 100;
+                document.querySelector("#percentageLtc").innerHTML = `(${percentage.toFixed(2)}%)`;
+              }
+            };
+            xhr.send();
+          }
+          
+          getLTCPrice();
+          
+    
+    /*----DOGE----*/
+            const url = 'https://min-api.cryptocompare.com/data/price?fsym=DOGE&tsyms=USD';
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById("doge-price").innerHTML = `DOGE $${data.USD}`;
+
+        const standardCurrency = 0.081286;
+                const currentPrice = `${data.USD}`;
+                const change = currentPrice - standardCurrency;
+                const percentage = (change / standardCurrency) * 100;
+                document.querySelector("#percentageDoge").innerHTML = `(${percentage.toFixed(2)}%)`;
+      });
+      
   
+  
+      /*----YFI----*/
+      function getYFIPrice() {
+      const url = 'https://min-api.cryptocompare.com/data/price?fsym=YFI&tsyms=USD';
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        document.querySelector("#YFI-price").innerHTML = `YFI $${data.USD}`;
+
+        const standardCurrency = 6765.46;
+                const currentPrice = `${data.USD}`;
+                const change = currentPrice - standardCurrency;
+                const percentage = (change / standardCurrency) * 100;
+                document.querySelector("#percentageYFI").innerHTML = `(${percentage.toFixed(2)}%)`;
+      });
+      };
+      getYFIPrice();
+    
+    
+    /*----LINK----*/
+    function getLinkPrice(){
+      const url = 'https://min-api.cryptocompare.com/data/price?fsym=LINK&tsyms=USD';
+    
     fetch(url)
       .then(response => response.json())
       .then(data => {
         document.querySelector("#link-price").innerHTML = `LINK $${data.USD}`;
-        localStorage.setItem("linkPrice", data.USD);
+
+        const standardCurrency = 6.61;
+                const currentPrice = `${data.USD}`;
+                const change = currentPrice - standardCurrency;
+                const percentage = (change / standardCurrency) * 100;
+                document.querySelector("#percentageLink").innerHTML = `(${percentage.toFixed(2)}%)`;
       });
+    }
+    getLinkPrice();
+  
   }
   
-  window.onload = function() {
-    var url1 = 'https://min-api.cryptocompare.com/data/price?fsym=LINK&tsyms=USD';
-
-  
-    fetch(url1)
-      .then(response => response.json())
-      .then(data => {
-        const currentPrice = data.USD;
-        if (localStorage.getItem("linkPrice")) {
-            const change = currentPrice - localStorage.getItem("linkPrice");
-      const percentageChange = (change / localStorage.getItem("linkPrice")) * 100;
-            document.querySelector("#percentage-change").innerHTML = `(${percentageChange.toFixed(2)}%)`;
-         } 
-      });
-      
-  };
-  setTimeout(function() {
-    window.location.assign(window.location.href);
-  }, 25000);
+    setInterval(nextLoad, 5000);
